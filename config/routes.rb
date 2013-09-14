856d481fca684dfbb02b0b2a91118fb8a839180e
qiuxiaox <qiuxiaox@cn.ibm.com>
2013-09-14 16:24:25 +0800

    add sign up page and tests

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 DemoApp::Application.routes.draw do
+  get "users/new"
   match '/', to: 'static_pages#home', via: 'get'
 
   root to: 'static_pages#home'
-
+  
+  match 'signup', to: 'users#new', via: 'get'
   match '/help',    to: 'static_pages#help',    via: 'get'
   match '/about',   to: 'static_pages#about',   via: 'get'
   match '/contact', to: 'static_pages#contact', via: 'get'
