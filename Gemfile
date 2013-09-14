2b46011f0b29b9aa51844a4d27ce5360d4744123
qiuxiaox <qiuxiaox@cn.ibm.com>
2013-09-14 23:34:33 +0800

    add use and password feature

--- Gemfile
+++ Gemfile
@@ -5,6 +5,7 @@ source 'http://ruby.taobao.org/'
 gem 'rails', '4.0.0'
 
 gem 'bootstrap-sass', '2.3.2.0'
+gem 'bcrypt-ruby', '3.0.1'
 
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3'
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130911084645) do
+ActiveRecord::Schema.define(version: 20130914151335) do
 
   create_table "microposts", force: true do |t|
     t.string   "content"
@@ -27,5 +27,7 @@ ActiveRecord::Schema.define(version: 20130911084645) do
     t.datetime "updated_at"
     t.string   "password_digest"
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
