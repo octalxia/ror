b652d94ed3a02515830e5d220feee8cddf7961dd
qiuxiaox <qiuxiaox@cn.ibm.com>
2013-09-20 17:15:35 +0800

    update user profile and control

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,30 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:edit, :update]  
+  before_action :correct_user,   only: [:edit, :update]
+  
   def new
     @user = User.new
   end
   
   def show
     @user = User.find(params[:id])
+  end
+  
+  def edit
+    # @user is defined in correct_user
+    # @user = User.find(params[:id])
+  end
+  
+  def update
+    # @user is defined in correct_user
+    #@user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      # Handle a successful update.
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
   
   def create
@@ -18,9 +38,23 @@ class UsersController < ApplicationController
     end
   end
   
+  private
+  
   def user_params
     params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
+  end
+  
+  def signed_in_user
+    unless signed_in?
+      store_location
+      redirect_to signin_url, notice: "Please sign in."
+    end
+  end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    redirect_to(root_path) unless current_user?(@user)
   end
   
 end
