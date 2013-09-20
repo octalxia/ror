cb01a8c29ea018424048dbd1a85850be91171a0a
qiuxiaox <qiuxiaox@cn.ibm.com>
2013-09-20 20:57:34 +0800

    delete user with admin permission

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:edit, :update]  
+  before_action :signed_in_user, only: [:index, :edit, :update]  
   before_action :correct_user,   only: [:edit, :update]
+  before_action :admin_user,     only: :destroy
   
   def index
     @users = User.paginate(page: params[:page])
@@ -41,24 +42,34 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User destroyed."
+    redirect_to users_url
+  end
   
   private
   
-  def user_params
-    params.require(:user).permit(:name, :email, :password,
+    def user_params
+      params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
-  end
-  
-  def signed_in_user
-    unless signed_in?
-      store_location
-      redirect_to signin_url, notice: "Please sign in."
     end
-  end
   
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_path) unless current_user?(@user)
-  end
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+      end
+    end
+  
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) unless current_user?(@user)
+    end
+  
+    def admin_user
+      redirect_to(root_path) unless current_user.admin?
+    end  
   
 end
--- lib/tasks/sample_data.rake
+++ lib/tasks/sample_data.rake
@@ -4,15 +4,17 @@ namespace :db do
     User.create!(name: "Sean Qiu",
                  email: "sean@gmail.com",
                  password: "123456",
-                 password_confirmation: "123456")
+                 password_confirmation: "123456",
+                 admin: true)
     99.times do |n|
       name  = Faker::Name.name
-      email = "example-#{n+1}@railstutorial.org"
+      email = "example-#{n+1}@gmail.com"
       password  = "password"
       User.create!(name: name,
                    email: email,
                    password: password,
-                   password_confirmation: password)
+                   password_confirmation: password,
+                   admin: false)
     end
   end
 end
